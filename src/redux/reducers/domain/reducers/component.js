de4dc30cd2a5ba98400f1359600e9f97a096f28c
denizsokullu <denizaydinsokullu@gmail.com>
2018-08-06 18:02:58 +0300

    added removeComponent reducer

--- src/redux/reducers/domain/reducers/component.js
+++ src/redux/reducers/domain/reducers/component.js
@@ -33,63 +33,19 @@ export const addComponent = (tabs,action) => {
 
   target.components = _.merge(target.components,allTheComponentsToAdd);
 
-  // let componentTree = findComponentTree(page,id);
+  return { ...tabs };
 
-  // console.log(component,componentTree);
+}
 
-  //a component can be added from the reactive components as well
-  //so make sure that there is a check for that later
-
-  //call a function to transform(create a copy of the component and all its subtree)
-
-  //this should work as
-  //find the component, give it a new id,
-  //find its tree structure, as you go down, copy and give as well as adding to the list of components.
-  //then add the component tree to the existing artboards tree,
-  //merge the componenent collection with the component collection of the artboard
-  //return the tab back.
-
-  //add the component
-
-  //how to handle links
-
-  //links are stored in the trigger component
-  //and fire of a change in the other element.
-
-  // links = {
-  //   linkId:{
-  //     trigger:{
-  //       id:'id of the trigger component'
-  //       method:'click'
-  //     },
-  //     targets:[
-  //       {
-  //         id:'id of the target component',
-  //         change:[
-  //           {
-  //             type:'position,style-bg-color',
-  //             value:'value of the property change'
-  //             timing:{
-  //               'props for timing go here'
-  //             }
-  //           },
-  //         ]
-  //       }
-  //     ]
-  //   },
-  //   linkId...,
-  //   linkId...,
-  // }
-
-  // //add it to the list of tabs
-  // tabs.allTabs[newTab.id] = newTab;
-  //
-  // //switch the active tab to the new tab
-  // tabs.activeTab = newTab.id;
-  //
-  // // increment the tabCount
-  // tabs.tabCount += 1;
-
+export const removeComponent = (tabs,action) => {
+  let domain = action.domain;
+  let payload = action.payload;
+  let target = tabs.allTabs[domain.tabs.activeTab];
+  let component = target.components[payload.id];
+  let allTheComponentsToDelete = traverseAndAdd(component,target.components,{});
+  Object.keys(allTheComponentsToDelete).map( comp => {
+    delete target.components[comp.id]
+  })
   return { ...tabs };
 }
 
